067717f9b472bb36ca5c3933405f70aa38b7d3e0
Jéssica Garcia <jessica.rodriguesgarcia@gmail.com>
2023-08-14 12:19:41 -0300

    responsive design and breakpoints

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,7 @@
 export default function Home() {
   return (
     <div className="p-8 bg-slate-900 text-slate-100 h-screen">
-      <h1 className="font-bold flex items-center gap-3 text-5xl before:flex before:w-0.5 before:h-8 before:bg-sky-500">Title</h1>
+      <h1 className="font-bold flex items-center gap-3 text-3xl sm:text-5xl lg:text-6xl before:flex before:w-0.5 before:h-8 before:bg-sky-500">Title</h1>
       <h2>Subtitle</h2>
       <p>Text</p>
       <button disabled className="bg-sky-500 px-4 py-2 rounded-md font-medium mt-4 enabled:hover:bg-sky-600 disabled:opacity-60 disabled:cursor-not-allowed">Sign in</button>
